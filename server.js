49716de6289a92635364b6772fb189183703477d
Pyae Sone Khant <pyaesonekhant@tumawlamyine.edu.mm>
2020-08-26 21:25:19 +0630

    Change configuration for production

--- server.js
+++ server.js
@@ -21,11 +21,15 @@ app.use("/api/user", user);
 
 // Configure For Production
 if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
+   app.use(express.static(path.resolve(__dirname, "client", "build")));
    app.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
    });
 }
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//    res.sendFile(path.join(__dirname, "../build"));
+// });
 
 // Configure the Mongo DB
 const PORT = process.env.PORT || 5000;
